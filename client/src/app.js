59dba5162a9e1ebfc7d9743627db69bb7dd23bb8
ghr85 <ghrenfrew@gmail.com>
2019-03-09 12:28:15 +0000

    bugfix: Incorrectly referenced quizview instead of resultview

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,7 @@ const Model = require('./models/model.js');
 const LandingView = require('./views/landing_view.js');
 const QuestionView = require('./views/question_view.js');
 const QuizView = require('./views/quiz_view.js');
-const ResultView = require('./views/quiz_view.js');
+const ResultView = require('./views/result_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 console.log('dom loaded successfully');
