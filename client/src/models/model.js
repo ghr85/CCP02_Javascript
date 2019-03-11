78fc60f6473ae6193194ab67bad29969427878e4
kiranqureshi1 <kiranqureshi@outlook.com>
2019-03-11 12:00:12 +0000

    factoids file added and working

--- client/src/models/model.js
+++ client/src/models/model.js
@@ -1,9 +1,27 @@
+const Factoids = require('./factoids.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+
 const Model = function(){
 
 };
 
 Model.prototype.bindEvent = function () {
   console.log('Model Integrated');
+  this.get_landing_quote();
+};
+
+
+Model.prototype.get_landing_quote = function () {
+ const factoids = new Factoids();
+ console.log(factoids);
+  const factoidString = factoids.factoidsArray[Math.floor(Math.random() * factoids.factoidsArray.length)];
+  console.log(factoidString);
+  return factoidString;
+};
+
+Model.prototype.getQuestionView = function () {
+
 };
 
 module.exports = Model;
