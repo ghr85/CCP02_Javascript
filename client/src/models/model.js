f395712dc4e086e18aa9c759a445fa73b3a39ec7
Ethan Radcliffe <ethanr@mac.com>
2019-03-12 15:30:43 +0000

    Wrong answers displayed on result view

--- client/src/models/model.js
+++ client/src/models/model.js
@@ -10,6 +10,7 @@ const Model = function(url){
   this.questionNumber = 0;
   this.currentQuestion = null;
   this.userScore = 0;
+  this.inCorrectQuestionArray = [];
 };
 
 Model.prototype.bindEvent = function () {
@@ -23,71 +24,77 @@ Model.prototype.bindEvent = function () {
   });
   PubSub.subscribe('QuestionView:answerselected', (evt) => {
     if (this.currentQuestion.correct == evt.detail ) {
-      console.log(evt.detail);
       this.userScore += 1;
-    };
+    } else {
+      this.inCorrectQuestionArray.push({
+        'userAnswer': evt.detail,
+        'answer': this.currentQuestion.correct,
+        'question': this.currentQuestion.question
+      })
+    }
     if (this.questionNumber < 10) {
       this.getQuestion();
     }
     else {
-      PubSub.publish('Model:FinalScore', this.userScore );
+      PubSub.publish('Model:FinalScore', {
+        'score': this.userScore,
+        'incorrectAnswers': this.inCorrectQuestionArray});
+      };
 
-    };
-
-  });
-  PubSub.subscribe('ResultView:restart-click', (evt) => {
-    this.reset();
-  })
-};
-
-Model.prototype.get_landing_quote = function () {
-  const factoids = new Factoids();
-  const factoidString = factoids.factoidsArray[Math.floor(Math.random() * factoids.factoidsArray.length)];
-  return factoidString;
-
-};
-
-Model.prototype.getQuestionData = function () {
-  this.request.get()
-  .then((data) => {
-    const shuffledQuestions = this.shuffle(data);
-    this.allQuestionsArray = shuffledQuestions.map((object)=>{
-      return {
-        "question":object.question,
-        "answer_count": object.incorrect_answers.push(object.correct_answer),
-        "correct": object.correct_answer,
-        "answers": this.shuffle(object.incorrect_answers)
-      }
     });
-  });
-};
-Model.prototype.shuffle = function (array) {
-  let currentIndex = array.length
-  while(--currentIndex > 0){
-    let randomIndex = Math.floor(Math.random() * (currentIndex+1))
-    let temporaryHold = array[randomIndex];
-    array[randomIndex] = array[currentIndex]
-    array[currentIndex] = temporaryHold
-  }
-  return array;
-};
+    PubSub.subscribe('ResultView:restart-click', (evt) => {
+      this.reset();
+    })
+  };
 
-Model.prototype.getQuestion = function () {
-  this.questionNumber += 1;
-  this.currentQuestion = this.allQuestionsArray[this.questionNumber];
-  PubSub.publish("Model:question-loaded", {
-    "questionNumber": this.questionNumber,
-    "question": this.currentQuestion
-  });
-};
+  Model.prototype.get_landing_quote = function () {
+    const factoids = new Factoids();
+    const factoidString = factoids.factoidsArray[Math.floor(Math.random() * factoids.factoidsArray.length)];
+    return factoidString;
 
-Model.prototype.reset = function () {
-  this.questionNumber = 0;
-  this.userScore = 0;
-  this.getQuestionData();
-  PubSub.publish('Model:Factoid-loaded', this.get_landing_quote())
-};
+  };
+
+  Model.prototype.getQuestionData = function () {
+    this.request.get()
+    .then((data) => {
+      const shuffledQuestions = this.shuffle(data);
+      this.allQuestionsArray = shuffledQuestions.map((object)=>{
+        return {
+          "question":object.question,
+          "answer_count": object.incorrect_answers.push(object.correct_answer),
+          "correct": object.correct_answer,
+          "answers": this.shuffle(object.incorrect_answers)
+        }
+      });
+    });
+  };
+  Model.prototype.shuffle = function (array) {
+    let currentIndex = array.length
+    while(--currentIndex > 0){
+      let randomIndex = Math.floor(Math.random() * (currentIndex+1))
+      let temporaryHold = array[randomIndex];
+      array[randomIndex] = array[currentIndex]
+      array[currentIndex] = temporaryHold
+    }
+    return array;
+  };
+
+  Model.prototype.getQuestion = function () {
+    this.questionNumber += 1;
+    this.currentQuestion = this.allQuestionsArray[this.questionNumber];
+    PubSub.publish("Model:question-loaded", {
+      "questionNumber": this.questionNumber,
+      "question": this.currentQuestion
+    });
+  };
+
+  Model.prototype.reset = function () {
+    this.questionNumber = 0;
+    this.userScore = 0;
+    this.getQuestionData();
+    PubSub.publish('Model:Factoid-loaded', this.get_landing_quote())
+  };
 
 
 
-module.exports = Model;
+  module.exports = Model;
--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -17,14 +17,34 @@ ResultView.prototype.bindEvent = function () {
     resultContainer.appendChild(heading);
 
     const result = document.createElement('p');
-    heading.textContent = `You got ${evt.detail} out of 10!`
-    resultContainer.appendChild(heading);
+    result.textContent = `You got ${evt.detail.score} out of 10!`
+    resultContainer.appendChild(result);
+    
+    if (evt.detail.score !== 10){
+      const wrongUns = document.createElement('p');
+      wrongUns.textContent = `Here are the questions you got wrong`
+      resultContainer.appendChild(wrongUns);
+      evt.detail.incorrectAnswers.forEach((element) => {
+        const incorrectQuestion = document.createElement('p')
+        incorrectQuestion.textContent = `${element.question}`
+        incorrectQuestion.classList.add('incorrectQ')
+        const incorrectAnswer = document.createElement('p')
+        incorrectAnswer.textContent = `You answered ${element.userAnswer}`
+        incorrectAnswer.classList.add('incorrectA')
+        const correctAnswer = document.createElement('p')
+        correctAnswer.textContent = `The correct answer is ${element.answer}`
+        correctAnswer.classList.add('correctA')
+        resultContainer.appendChild(incorrectQuestion);
+        resultContainer.appendChild(incorrectAnswer);
+        resultContainer.appendChild(correctAnswer);
+      })
+    };
 
     const restartButton = this.createRestartButton();
     resultContainer.appendChild(restartButton);
 
     this.container.appendChild(resultContainer);
-  } );
+  });
 };
 
 ResultView.prototype.createRestartButton = function () {
