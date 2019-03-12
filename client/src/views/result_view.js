15b4c622fa68284a87fa46508dc20489690e9012
kiranqureshi1 <kiranqureshi@outlook.com>
2019-03-12 16:49:56 +0000

    corrected merge conflicts in result view

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -1,12 +1,8 @@
-const Highcharts = require('Highcharts');
-
 const PubSub = require('../helpers/pub_sub.js');
-
+const Highcharts = require('highcharts')
 const ResultView = function(container){
   this.container = container;
 };
-
-
 
 ResultView.prototype.bindEvent = function () {
   console.log('ResultView Integrated');
@@ -20,74 +16,111 @@ ResultView.prototype.bindEvent = function () {
     heading.textContent = 'Result...'
     resultContainer.appendChild(heading);
 
-    const result = document.createElement('p');
-    result.textContent = `You got ${evt.detail.score} out of 10!`
-    resultContainer.appendChild(result);
-
-    if (evt.detail.score !== 10){
-      const wrongUns = document.createElement('p');
-      wrongUns.textContent = `Here are the questions you got wrong`
-      resultContainer.appendChild(wrongUns);
-      evt.detail.incorrectAnswers.forEach((element) => {
-        const incorrectQuestion = document.createElement('p')
-        incorrectQuestion.textContent = `${element.question}`
-        incorrectQuestion.classList.add('incorrectQ')
-        const incorrectAnswer = document.createElement('p')
-        incorrectAnswer.textContent = `You answered ${element.userAnswer}`
-        incorrectAnswer.classList.add('incorrectA')
-        const correctAnswer = document.createElement('p')
-        correctAnswer.textContent = `The correct answer is ${element.answer}`
-        correctAnswer.classList.add('correctA')
-        resultContainer.appendChild(incorrectQuestion);
-        resultContainer.appendChild(incorrectAnswer);
-        resultContainer.appendChild(correctAnswer);
-      })
-    };
-
-    const restartButton = this.createRestartButton();
-    resultContainer.appendChild(restartButton);
-
     Highcharts.chart(this.container, {
-        chart: {
-            renderTo: 'container',
-            type: 'pie'
-        },
-        title: {
-            text: 'Your Score'
-        },
-        plotOptions: {
-       pie: {
-           allowPointSelect: true,
-           cursor: 'pointer',
-           dataLabels: {
-               enabled: true,
-               format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-               style: {
-                   color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'brown',
-                   backgroundColor: (Highcharts.theme && Highcharts.theme.contrastBackgroundColor) || 'red'
-               }
-           }
-       }
-        },
-        series: [{
+      chart: {
+        renderTo: 'container',
+        type: 'pie'
+      },
+      title: {
+        text: 'Your Score'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            style: {
+              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'brown',
+              backgroundColor: (Highcharts.theme && Highcharts.theme.contrastBackgroundColor) || 'red'
+            }
+          }
+        }
+      },
+      series: [{
 
-            name: 'result',
-            data: [{
-            name: "correct",
-            color: "green",
-            y: evt.detail.score
+        name: 'result',
+        data: [{
+          name: "correct",
+          color: "green",
+          y: evt.detail.score
 
         }, {
-            name: 'Incorrect',
-            color: "red",
-            y: 10 - evt.detail.score
+          name: 'Incorrect',
+          color: "red",
+          y: 10 - evt.detail.score
         }
       ]
     }]
   })
-  this.container.appendChild(resultContainer);
 
-} );
+
+const result = document.createElement('p');
+result.textContent = `You got ${evt.detail.score} out of 10!`
+resultContainer.appendChild(result);
+
+const scoreComment = this.checkScore(evt.detail.score);
+resultContainer.appendChild(scoreComment);
+
+if (evt.detail.score !== 10){
+  const wrongUns = document.createElement('p');
+  wrongUns.textContent = `Here are the questions you got wrong`
+  resultContainer.appendChild(wrongUns);
+  evt.detail.incorrectAnswers.forEach((element) => {
+    const incorrectQuestion = document.createElement('p')
+    incorrectQuestion.textContent = `${element.question}`
+    incorrectQuestion.classList.add('incorrectQ')
+
+    const incorrectAnswer = document.createElement('p')
+    incorrectAnswer.textContent = `You answered ${element.userAnswer}`
+    incorrectAnswer.classList.add('incorrectA')
+
+    const correctAnswer = document.createElement('p')
+    correctAnswer.textContent = `The correct answer is ${element.answer}`
+    correctAnswer.classList.add('correctA')
+
+    resultContainer.appendChild(incorrectQuestion);
+    resultContainer.appendChild(incorrectAnswer);
+    resultContainer.appendChild(correctAnswer);
+  })
+};
+
+const restartButton = this.createRestartButton();
+resultContainer.appendChild(restartButton);
+
+
+this.container.appendChild(resultContainer);
+
+});
+};
+
+ResultView.prototype.checkScore = function (score) {
+  const scoreText = document.createElement('p')
+  scoreText.classList.add('scoreComment')
+  if (score > 0 && score <= 3)
+  {
+    scoreText.textContent = `Get Yourself Back to School!`
+  } else if (score > 3 && score <= 6) {
+    scoreText.textContent = 'You Need to Brush Up!'
+  }
+  else if (score > 6 && score <= 9)
+  {
+    scoreText.textContent = 'A for Effort!'
+  }
+  else if (score == 10)
+  {
+    scoreText.textContent = 'You Smell Like Trees!'
+  }
+  else if (score === 0)
+  {
+    scoreText.textContent = `You're a Planet Killer!`
+  }
+
+
+  return scoreText;
+
+
 
 };
 
@@ -103,6 +136,5 @@ ResultView.prototype.createRestartButton = function () {
   });
   return button;
 };
-
 
 module.exports = ResultView;
