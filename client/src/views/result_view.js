dd1bbea68cb4ece871dfcdbe8126bb5431d9f769
Ethan Radcliffe <ethanr@mac.com>
2019-03-12 16:03:07 +0000

    score comment added

--- client/src/views/result_view.js
+++ client/src/views/result_view.js
@@ -19,7 +19,10 @@ ResultView.prototype.bindEvent = function () {
     const result = document.createElement('p');
     result.textContent = `You got ${evt.detail.score} out of 10!`
     resultContainer.appendChild(result);
-    
+
+    const scoreComment = this.checkScore(evt.detail.score);
+    resultContainer.appendChild(scoreComment);
+
     if (evt.detail.score !== 10){
       const wrongUns = document.createElement('p');
       wrongUns.textContent = `Here are the questions you got wrong`
@@ -28,12 +31,15 @@ ResultView.prototype.bindEvent = function () {
         const incorrectQuestion = document.createElement('p')
         incorrectQuestion.textContent = `${element.question}`
         incorrectQuestion.classList.add('incorrectQ')
+
         const incorrectAnswer = document.createElement('p')
         incorrectAnswer.textContent = `You answered ${element.userAnswer}`
         incorrectAnswer.classList.add('incorrectA')
+
         const correctAnswer = document.createElement('p')
         correctAnswer.textContent = `The correct answer is ${element.answer}`
         correctAnswer.classList.add('correctA')
+
         resultContainer.appendChild(incorrectQuestion);
         resultContainer.appendChild(incorrectAnswer);
         resultContainer.appendChild(correctAnswer);
@@ -45,6 +51,35 @@ ResultView.prototype.bindEvent = function () {
 
     this.container.appendChild(resultContainer);
   });
+};
+
+ResultView.prototype.checkScore = function (score) {
+  const scoreText = document.createElement('p')
+  scoreText.classList.add('scoreComment')
+  if (score > 0 && score <= 3)
+  {
+    scoreText.textContent = `Get Yourself Back to School!`
+  } else if (score > 3 && score <= 6) {
+    scoreText.textContent = 'You Need to Brush Up!'
+  }
+  else if (score > 6 && score <= 9)
+  {
+    scoreText.textContent = 'A for Effort!'
+  }
+  else if (score == 10)
+  {
+    scoreText.textContent = 'You Smell Like Trees!'
+  }
+  else if (score === 0)
+  {
+    scoreText.textContent = `You're a Planet Killer!`
+  }
+
+
+  return scoreText;
+
+
+
 };
 
 ResultView.prototype.createRestartButton = function () {
