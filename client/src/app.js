212277fd67f02012325993f7df3c65c4b0c8ebc9
kiranqureshi1 <kiranqureshi@outlook.com>
2019-03-14 09:52:48 +0000

    app to older version

--- client/src/app.js
+++ client/src/app.js
@@ -1,30 +1,3 @@
-// const PubSub = require('./helpers/pub_sub.js');
-// const Model = require('./models/model.js');
-// const LandingView = require('./views/landing_view.js');
-// const QuestionView = require('./views/question_view.js');
-// const QuizView = require('./views/quiz_view.js');
-// const ResultView = require('./views/result_view.js');
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log('dom loaded successfully');
-//
-//   const viewContainer = document.querySelector('#content_parent');
-//   const landingView = new LandingView(viewContainer);
-//   const model = new Model('http://localhost:3000/api/questions');
-//   const infoDivA = document.querySelector('#question-number')
-//   const quizView = new QuizView(infoDivA);
-//   const questionView = new QuestionView(viewContainer);
-//   const resultView = new ResultView(infoDivA);
-//
-//   landingView.bindEvent();
-//   model.bindEvent();
-//   questionView.bindEvent();
-//   quizView.bindEvent();
-//   resultView.bindEvent();
-//
-// });
-
-
 const PubSub = require('./helpers/pub_sub.js');
 const Model = require('./models/model.js');
 const LandingView = require('./views/landing_view.js');
